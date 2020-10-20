d16922c81ed03070996d45624129e9b83a10d847
prasannadhapate <prasannadhapate@gmail.com>
2020-10-20 15:11:55 +0530

    done some integreations again

--- login-reg pages/mysqli_connect.php
+++ login-reg pages/mysqli_connect.php
@@ -15,5 +15,4 @@ $conn = new mysqli($DB_HOST, $DB_USER , $DB_PASSWORD , $DB_NAME );
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-  echo "Connected successfully";
   ?>
--- src/Register.php
+++ src/Register.php
@@ -1,13 +1,69 @@
-<?php
-//header.php
-include('header.php');
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+       content=" width=device-width,user-scalable=no.initia-scale=1.0,maximum-scale=1.0,minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible"content="ie=edge">
+    <title>Registration</title>
 
-?>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="../css/login.css">
+
+    </head>
+    <style>
+         #register{
+    padding:10%0;
+    background: url("../assets/login/food1.png")no-repeat;
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    animation: scale 3s;
+}
+      #footer {
+      margin-top:28px;
+      background-color: #141414;
+      color: #f5f5f5;
+      padding-top: 2rem;
+  }
+    </style>
+
+    <body>
+    <nav class="navbar navbar-expand-md navbar-light bg-transparent">
+      <!--md break point at 768px-->
+      <div class="container-fluid">
+        <!-- container fluid takes up 100% of the screen-->
+        
+        <h3 id="headtext">DY PATIL COLLEGE OF ENGINEERING</h3>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+          <!--thsi is navigation toggler for smaller screens-->
+          <span class="navbar-toggler-icon"></span>
+          <!--this is for the navbar icon-->
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="../index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./menu.html">Menu</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="./menu.html">About Us</a>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
+    </nav>
+   <main id="main_area">
 
 <?php
 
 if($_SERVER['REQUEST_METHOD']=='POST'){
-  require('register-process.php');
+  require('../login-reg pages/register-process.php');
 }
 
 
@@ -20,7 +76,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     <div class="text-center pb-5 ">
     <h1 class="login-title text-light ">Register</h1>
     <p class="p-1 m-0 font-ubuntu text-light ">Register and enjoy additional features</p>
-    <span class="font-ubuntu text-light">I already have<a href="login.php">Login</a></span>
+    <span class="font-ubuntu text-light">I already have<a href="login.php">&nbsp;Login</a></span>
     </div>
 
     <div class="d-flex justify-content-center">
@@ -83,8 +139,33 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 </section>
  <!--registration end-->
 
- <?php
-//footer.php
-include('footer.php');
+ </main>
+ <footer id="footer" class="p-5">
+    <div class="container-fluid padding">
+      <div class="row">
+        <div class="col-lg-4">
+          <h4 class="font-weight-bold">DY PATIL COLLEGE OF ENGINEERING</h4>
+          <hr class="light">
+          <p>Good Food leads to a Good Mood</p>
+        </div>
+        <div class="col-lg-4 d-flex justify-content-center">
+          <div>
+            <h3>Timings</h3>
+            <hr class="light">
+            <p>8:00 am - 6:00 pm</p>
+          </div>
+        </div>
 
-?>
+        <div class="col-lg-4 d-flex justify-content-center">
+          <div>
+            <h3>For feeback</h3>
+            <hr class="light">
+            <p>Email: info@dypcoeakurdi.ac.in</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+      </body>
+  
+      </html>
